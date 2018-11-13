e801eae09899773c70704f662dce38efc16f9a21
Tom Pritchard <tompritchard1996@gmail.com>
2018-11-13 19:45:43 +0000

    Switch to sockets for easier debugging

--- MKAgent/src/main/java/org/AIandGames/mancalabot/Main.java
+++ MKAgent/src/main/java/org/AIandGames/mancalabot/Main.java
@@ -5,6 +5,9 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.PrintWriter;
 
 /**
  * The main application class. It also provides methods for communication
@@ -15,7 +18,14 @@ public class Main
     /**
      * Input from the game engine.
      */
-    private static Reader input = new BufferedReader(new InputStreamReader(System.in));
+
+	//The actual server expects the client to be running and waiting, and java sockets 
+	//...expect the server to be running and waiting... Set up a Server that just listens
+	//...so the client and server don't time out as a result.
+	private static ServerSocket server;
+	private static Socket clientSocket;	
+	private static Reader input;
+	private static PrintWriter output;
 
     /**
      * Sends a message to the game engine.
@@ -24,7 +34,10 @@ public class Main
     public static void sendMsg (String msg)
     {
     	System.out.print(msg);
-    	System.out.flush();
+		System.out.flush();
+		
+		output.print(msg);
+		output.flush();
     }
 
     /**
@@ -59,6 +72,11 @@ public class Main
 
 		try
 		{
+			server = new ServerSocket(12345);	//Setup a server on localhost, port 12345.
+			clientSocket = server.accept();	//Client socket on port 12345.
+			input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		 	output = new PrintWriter(clientSocket.getOutputStream(), true);
+
 			String s;
 			while (true)
 			{
@@ -74,7 +92,7 @@ public class Main
 							System.err.println("Starting player? " + first);
 							break;
 						case STATE: System.err.println("A state.");
-							org.AIandGames.mancalabot.Board b = new org.AIandGames.mancalabot.Board(6,6);
+							org.AIandGames.mancalabot.Board b = new org.AIandGames.mancalabot.Board(7, 7);
 							org.AIandGames.mancalabot.Protocol.MoveTurn r = org.AIandGames.mancalabot.Protocol.interpretStateMsg (s, b);
 							System.err.println("This was the move: " + r.move);
 							System.err.println("Is the game over? " + r.end);
