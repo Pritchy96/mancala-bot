0db92e90547a40fb2801f78fbb2a70f6c434bfc4
BillLynch <llynch.bill@gmail.com>
2018-12-09 02:40:36 +0000

    repeat_move_heuristic

---
+++ MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/RepeatMoveAvailable.java
@@ -0,0 +1,38 @@
+package org.AIandGames.mancalabot.Heuristics;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.AIandGames.mancalabot.Enums.Heuristics;
+import org.AIandGames.mancalabot.Enums.Side;
+import org.AIandGames.mancalabot.GameTreeNode;
+import org.AIandGames.mancalabot.Kalah;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RepeatMoveAvailable implements Heuristic {
+    private final GameTreeNode node;
+
+    @Override
+    public Heuristics getName() {
+        return Heuristics.REPEAT_MOVE_AVAILABLE;
+    }
+
+    @Override
+    public int getValue(final Side ourSide) {
+        final int northSideReturn = Side.NORTH.equals(ourSide) ? 1 : -1;
+        final int southSideReturn = Side.SOUTH.equals(ourSide) ? 1 : -1;
+
+        final Kalah kalah = new Kalah(this.node.getBoard());
+
+        return MovesStaticList.MOVES_LIST.stream()
+                .filter(kalah::isLegalMove)
+                .mapToInt(move -> {
+                    int points = kalah.makeMove(move).equals(Side.NORTH) ? northSideReturn : 0;
+                    points += kalah.makeMove(move).equals(Side.SOUTH) ? southSideReturn : 0;
+                    return points;
+                })
+                .reduce(0, (acc, point) -> acc + point);
+    }
+}
