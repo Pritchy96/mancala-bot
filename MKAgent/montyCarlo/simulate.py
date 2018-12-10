838feae64970805406604b11bfcc8d32a96c43ff
BillLynch <llynch.bill@gmail.com>
2018-12-10 15:46:08 +0000

    running logic implemented

--- MKAgent/montyCarlo/simulate.py
+++ MKAgent/montyCarlo/simulate.py
@@ -1,10 +1,21 @@
 import subprocess
 from collections import OrderedDict
+import math
+import random
 
 winDict = {}
 
+def deviate(w):
+    dev1 = random.triangular(0, 0.1, 0.05)
+    dev2 = random.triangular(0, 0.1, 0.05)
+    dev3 = random.triangular(0, 0.1, 0.05)
+    temp = w.split(" ")
+    wNew = " " + str(float(temp[1]) + dev1) + " " + str(float(temp[2]) + dev2) + " " + str(float(temp[3]) + dev3)
+    return wNew
+
 
 def play(w, wPrime):
+    print("playing")
     p = subprocess.run(args, capture_output=True)
     out = str(p.stderr).split("WINNER: ")
     wl = out[1].split("\\n")
@@ -24,22 +35,35 @@ args = ['java', '-jar', '../../ManKalah.jar',
 
 wAll = []
 
+
+
 for x in range(0, 50):
-    weight = " 0.1 0.2 0.3 0.5"
+    h1 = str(random.triangular(0, 1, 0.5))
+    h2 = str(random.triangular(0, 1, 0.5))
+    h3 = str(random.triangular(0, 1, 0.5))
+    weight = " " + h1 + " " + h2 + " " + h3
     wAll.append(weight)
     winDict[weight] = 0
 
-for x in range(1, 10):
+
+for x in range(1, 2):
     for w in wAll:
         for wPrime in wAll:
             if w is not wPrime:
                 play(w, wPrime)
-                play(wPrime, w)
-    winDict = OrderedDict(sorted(winDict.items()))
-    twentyPercent = len(winDict) * 0.2
+
+    print(winDict)
+    winDictSorted = OrderedDict(sorted(winDict.items()))
+    twentyPercent = int(math.ceil(len(winDictSorted) * 0.2))
+    print(twentyPercent)
     count = 0
-    for key, value in winDict.items():
-        if count <= twentyPercent:
+    for key, value in winDictSorted.items():
+        if count < twentyPercent:
             wAll.remove(key)
         else:
             break
+        count = count + 1
+
+    for w in wAll:
+        w = deviate(w)
+
