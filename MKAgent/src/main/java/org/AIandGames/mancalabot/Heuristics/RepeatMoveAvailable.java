01d4cb885553c8154dbbc40dd8ca74814da1e8e8
Nathan Oldfield <nathan@Nathans-MacBook-Air.local>
2018-12-11 16:16:22 +0000

    rewrote RepeatMoveAvailable to ensure it complies with heuristic return standards

--- MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/RepeatMoveAvailable.java
+++ MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/RepeatMoveAvailable.java
@@ -7,6 +7,7 @@ import org.AIandGames.mancalabot.Enums.Heuristics;
 import org.AIandGames.mancalabot.Enums.Side;
 import org.AIandGames.mancalabot.GameTreeNode;
 import org.AIandGames.mancalabot.Kalah;
+import org.AIandGames.mancalabot.Move;
 
 @AllArgsConstructor
 @EqualsAndHashCode
@@ -21,7 +22,7 @@ public class RepeatMoveAvailable implements Heuristic {
 
     @Override
     public int getValue(final Side ourSide) {
-        final int northSideReturn = Side.NORTH.equals(ourSide) ? 1 : -1;
+        /*final int northSideReturn = Side.NORTH.equals(ourSide) ? 1 : -1;
         final int southSideReturn = Side.SOUTH.equals(ourSide) ? 1 : -1;
 
         final Kalah kalah = new Kalah(this.node.getBoard());
@@ -30,5 +31,29 @@ public class RepeatMoveAvailable implements Heuristic {
                 .filter(kalah::isLegalMove)
                 .mapToInt(move -> kalah.makeMove(move).equals(Side.NORTH) ? northSideReturn : southSideReturn)
                 .reduce(0, (acc, point) -> acc + point);
+
+        */
+
+        final int isItOurTurnMultiplier = ourSide.equals(this.node.getCurrentSide()) ? 1 : -1;
+
+        int numberOfRepeatMovesAvailable = 0;
+
+        final Kalah kalah = new Kalah(this.node.getBoard());
+
+        for (int i = 1; i <= 7; i++) {
+            final Move move = new Move(this.node.getCurrentSide(), i);
+            if (this.moveGivesAnotherMove(kalah, move)) {
+                numberOfRepeatMovesAvailable += 1;
+            }
+        }
+
+        return numberOfRepeatMovesAvailable * isItOurTurnMultiplier;
+    }
+
+    private boolean moveGivesAnotherMove(final Kalah kalah, final Move move) {
+        if (kalah.isLegalMove(move) && (kalah.makeMove(move) == this.node.getCurrentSide())) {
+            return true;
+        }
+        return false;
     }
 }
