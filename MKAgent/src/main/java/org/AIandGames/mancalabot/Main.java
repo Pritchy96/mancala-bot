dcb9fb71e7b3ca87f54d4e846366ab060915b278
BillLynch <llynch.bill@gmail.com>
2018-12-12 16:36:58 +0000

    allow input of weights as args

--- MKAgent/src/main/java/org/AIandGames/mancalabot/Main.java
+++ MKAgent/src/main/java/org/AIandGames/mancalabot/Main.java
@@ -1,9 +1,20 @@
 package org.AIandGames.mancalabot;
 
 
+import org.AIandGames.mancalabot.Heuristics.HeuristicWeightings;
+
 public class Main {
 
     public static void main(final String[] args) {
+        final HeuristicWeightings heuristicWeightings = HeuristicWeightings.getInstance();
+
+        if (args.length == 5) {
+            heuristicWeightings.init(Double.parseDouble(args[0]),
+                    Double.parseDouble(args[1]),
+                    Double.parseDouble(args[2]),
+                    Double.parseDouble(args[3]),
+                    Double.parseDouble(args[4]));
+        }
         final GameRunner gr = new GameRunner();
         gr.run();
     }
