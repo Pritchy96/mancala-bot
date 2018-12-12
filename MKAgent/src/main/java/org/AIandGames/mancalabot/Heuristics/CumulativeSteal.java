5e42e3003d37451901055c24432f0b5ab1f5ef57
Tom Pritchard <tompritchard1996@gmail.com>
2018-12-12 13:35:28 +0000

    Implement fixes to CumulativeSteal

--- MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/CumulativeSteal.java
+++ MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/CumulativeSteal.java
@@ -3,7 +3,6 @@ package org.AIandGames.mancalabot.Heuristics;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
-import org.AIandGames.mancalabot.Board;
 import org.AIandGames.mancalabot.Enums.Heuristics;
 import org.AIandGames.mancalabot.Enums.Side;
 import org.AIandGames.mancalabot.GameTreeNode;
@@ -31,15 +30,16 @@ public class CumulativeSteal implements Heuristic {
         int valueOfSteals = 0;
 
         for (int i = 1; i <= 7; i++) {
-            if (this.thisHoleHasASteal(currentSide, i)) {
-                for (int j = 1; j < i; j++) {
-                    if (this.node.getBoard().getSeeds(currentSide, j) == i - j) {
-                        valueOfSteals += this.node.getBoard().getSeedsOp(currentSide, i) + 1;
+            if (this.thisHoleIsEmpty(currentSide, i)) {
+                if (this.thisHoleHasAPotentialSteal(currentSide, i)) {
+                    for (int j = 1; j < i; j++) {
+                        if (this.node.getBoard().getSeeds(currentSide, j) == i - j) {
+                            valueOfSteals += this.node.getBoard().getSeedsOp(currentSide, i) + 1;
+                        }
                     }
                 }
-            }
-            if (this.thisHoleHasALoopedSteal(currentSide, i)) {
-                for (int j = i; j <= 7; j++) {
+                
+                for (int j = i+1; j <= 7; j++) {
                     if (this.node.getBoard().getSeeds(currentSide, j) == 15 + i - j) { // LOOPED STEAL
                         valueOfSteals += this.node.getBoard().getSeedsOp(currentSide, i) + 2;
                     }
@@ -50,11 +50,11 @@ public class CumulativeSteal implements Heuristic {
         return valueOfSteals;
     }
 
-    private boolean thisHoleHasALoopedSteal(final Side currentSide, final int i) {
+    private boolean thisHoleIsEmpty(final Side currentSide, final int i) {
         return this.node.getBoard().getSeeds(currentSide, i) == 0;
     }
 
-    private boolean thisHoleHasASteal(final Side currentSide, final int i) {
-        return this.node.getBoard().getSeeds(currentSide, i) == 0 && this.node.getBoard().getSeedsOp(currentSide, i) > 0;
+    private boolean thisHoleHasAPotentialSteal(final Side currentSide, final int i) {
+        return this.node.getBoard().getSeedsOp(currentSide, i) > 0;
     }
 }
