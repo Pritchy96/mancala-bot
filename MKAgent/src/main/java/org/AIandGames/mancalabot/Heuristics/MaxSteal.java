26684e28f6697414378f7a5d2e6e93d83aac58d7
Tom Pritchard <tompritchard1996@gmail.com>
2018-12-12 13:26:23 +0000

    fix logical errors and PR requests

--- MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/MaxSteal.java
+++ MKAgent/src/main/java/org/AIandGames/mancalabot/Heuristics/MaxSteal.java
@@ -30,25 +30,23 @@ public class MaxSteal implements Heuristic {
         int maxStealValue = 0;
         final Board currentBoard = this.node.getBoard();
         for (int i = 1; i <= 7; i++) {
-            if (this.thisHoleHasASteal(currentSide, i)) {
-                for (int j = 1; j < i; j++) {
-                    if (currentBoard.getSeeds(currentSide, j) == i - j) {
-                        final int valueOfSteals = currentBoard.getSeedsOp(currentSide, i) + 1;
-                        if (maxStealValue < valueOfSteals) {
-                            maxStealValue = valueOfSteals;
+            if (this.thisHoleIsEmpty(currentSide, i)) {
+                if (this.thisHoleHasAPotentialSteal(currentSide, i)) {
+                    for (int j = 1; j < i; j++) {
+                        if (currentBoard.getSeeds(currentSide, j) == i - j) {
+                            final int valueOfSteals = currentBoard.getSeedsOp(currentSide, i) + 1;
+                            if (maxStealValue < valueOfSteals) {
+                                maxStealValue = valueOfSteals;
+                            }
                         }
-                        break;
                     }
                 }
-            }
-            if (this.thisHoleHasALoopedSteal(currentSide, i)) {
-                for (int j = i; j <= 7; j++) {
-                    if (currentBoard.getSeeds(currentSide, j) == i - j) {
-                        final int valueOfSteals = currentBoard.getSeedsOp(currentSide, i) + 1;
+                for (int j = i+1; j <= 7; j++) {
+                    if (currentBoard.getSeeds(currentSide, j) == 15+i-j) {
+                        final int valueOfSteals = currentBoard.getSeedsOp(currentSide, i) + 2;
                         if (maxStealValue < valueOfSteals) {
                             maxStealValue = valueOfSteals;
                         }
-                        break;
                     }
                 }
             }
@@ -57,11 +55,11 @@ public class MaxSteal implements Heuristic {
         return maxStealValue;
     }
 
-    private boolean thisHoleHasALoopedSteal(final Side currentSide, final int i) {
+    private boolean thisHoleIsEmpty(final Side currentSide, final int i) {
         return this.node.getBoard().getSeeds(currentSide, i) == 0;
     }
 
-    private boolean thisHoleHasASteal(final Side currentSide, final int i) {
-        return this.node.getBoard().getSeeds(currentSide, i) == 0 && this.node.getBoard().getSeedsOp(currentSide, i) > 0;
+    private boolean thisHoleHasAPotentialSteal(final Side currentSide, final int i) {
+        return this.node.getBoard().getSeedsOp(currentSide, i) > 0;
     }
 }
