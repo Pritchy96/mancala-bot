98c8880d2b2d01612a3b24eb9efc00361dbbae25
BillLynch <llynch.bill@gmail.com>
2018-12-12 18:34:16 +0000

    print the whole dict

--- MKAgent/src/geneticAlgo/geneticAlgo.py
+++ MKAgent/src/geneticAlgo/geneticAlgo.py
@@ -9,14 +9,19 @@ winDict = {}
 ourRunArg = 'java -jar ../../target/mancalaBot-1.0-SNAPSHOT-jar-with-dependencies.jar'
 
 def deviate(w):
-    dev1 = random.triangular(-0.1, 0.1, 0.0)
-    dev2 = random.triangular(-0.1, 0.1, 0.0)
-    dev3 = random.triangular(-0.1, 0.1, 0.0)
-    dev4 = random.triangular(-0.1, 0.1, 0.0)
-    dev5 = random.triangular(-0.1, 0.1, 0.0)
+    dev1 = random.triangular(-0.3, 0.3, 0.0)
+    dev2 = random.triangular(-0.3, 0.3, 0.0)
+    dev3 = random.triangular(-0.3, 0.3, 0.0)
+    dev4 = random.triangular(-0.3, 0.3, 0.0)
+    dev5 = random.triangular(-0.3, 0.3, 0.0)
     temp = w.split(" ")
     wNew = " " + str(float(temp[1]) + dev1) + " " + str(float(temp[2]) + dev2) + " " + str(float(temp[3]) + dev3) + " " + str(float(temp[4]) + dev4) + " " + str(float(temp[5]) + dev5)
     return wNew
+
+def deviate_one(w):
+    dev1 = random.triangular(-0.1, 0.1, 0.0)
+    return w + dev1
+
 
 
 def play(w, wPrime):
@@ -40,11 +45,11 @@ def play(w, wPrime):
 
 
 for x in range(0, nInInitialGeneration):
-    h1 = str(random.random())
-    h2 = str(random.random())
-    h3 = str(random.random())
-    h4 = str(random.random())
-    h5 = str(random.random())
+    h1 = str(random.triangular(0, 1, 0.5))
+    h2 = str(random.triangular(0, 1, 0.5))
+    h3 = str(random.triangular(0, 1, 0.5))
+    h4 = str(random.triangular(0, 1, 0.5))
+    h5 = str(random.triangular(0, 1, 0.5))
     weight = " " + h1 + " " + h2 + " " + h3 + " " + h4 + " " + h5
     winDict[weight] = 0
 
@@ -76,6 +81,8 @@ for x in range(1, nGenerations):
 
         winDict = newWinDict
 
+
+print(winDict)
 # now just get the 'best' of the resulting
 winDict = dict([(k, winDict[k]) for k in sorted(winDict, key=winDict.get, reverse=True)])
 for k, v in winDict.items():
